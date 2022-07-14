47a31f1cedc4603952ba49c2726c6bd7cc33227f
Joseph <joseph.vitale@outlook.com>
2022-07-13 20:11:28 -0400

    Cleaning up code

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 #Christian Znidarsic
 print("Hello world")
-<<<<<<< HEAD
-#Libby Lewis
-print('This is Libby's first commit')
-=======
 
+#Libby Lewis
+print("This is Libby's first commit")
+
+# Joseph Vitale
 print("Joseph's First Commit --> Hello world")
->>>>>>> c1367211ea4c6e59c783caee0cd9fe24f156013d
+
