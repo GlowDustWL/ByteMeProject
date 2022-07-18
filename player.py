b9227c73e055aa32605405ebb66b123adc970901
Libby Lewis <elewis43@jh.edu>
2022-07-18 13:01:01 -0400

    Added Free Token Method

--- player.py
+++ player.py
@@ -2,13 +2,12 @@ class Player():
     def __init__(self, player_name):
         self.name = player_name
         self.score = 0
+        self.free_token = 0
 
     def get_name(self):
-        print(self.name)
         return self.name
 
     def get_score(self):
-        print(self.score)
         return self.score
 
     def add_score(self, points):
@@ -19,3 +18,6 @@ class Player():
 
     def zero_score(self):
         self.score = 0
+
+    def add_token(self):
+        self.free_token += 1
