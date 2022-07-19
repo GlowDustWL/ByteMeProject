c11a73cfa9eb9b492c4482c4e229fd1b1714a0c9
Libby Lewis <elewis43@jh.edu>
2022-07-19 00:57:48 -0400

    Added LOG messages to player class

--- player.py
+++ player.py
@@ -1,23 +1,30 @@
 class Player():
     def __init__(self, player_name):
+        print("[LOG]: Player instance created.")
         self.name = player_name
         self.score = 0
         self.free_token = 0
 
     def get_name(self):
+        print("[LOG]: Player.get_name called.")
         return self.name
 
     def get_score(self):
+        print("[LOG]: Player.get_score called.")
         return self.score
 
     def add_score(self, points):
+        print("[LOG]: Player.add_score called.")
         self.score += points
 
     def sub_score(self, points):
+        print("[LOG]: Player.sub_score called.")
         self.score -= points
 
     def zero_score(self):
+        print("[LOG]: Player.zero_score called.")
         self.score = 0
 
     def add_token(self):
+        print("[LOG]: Player.add_token called.")
         self.free_token += 1
