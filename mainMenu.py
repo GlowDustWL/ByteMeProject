5602f8e9998dd9861396e76cac61ee7090ed9ea2
Joseph <joseph.vitale@outlook.com>
2022-08-17 20:48:19 -0400

    Updating Main Menu

--- mainMenu.py
+++ mainMenu.py
@@ -20,29 +20,31 @@ class MainMenu():
     # main menu
 
     def getInput(self):
-        text = textTitle.TextTitle("Wheel of Jeopardy", self.width/2, 150)
+        text = textTitle.TextTitle("Wheel of Jeopardy", self.width/2, 200)
+        subtitle = textDisplay.TextDisplay(
+            "Created by The ByteMe Team", 16, 1000, 250)
         # self.screen.blit(self.background, (0, 0))
         # pygame.display.update()
 
         # player number entry text
         playerNumText = textDisplay.TextDisplay(
-            "Select Number of Players", 48, self.width/2, self.height/2 - 120)
+            "Select Number of Players", 48, self.width/2, self.height/2 - 105)
 
         # buttons
         # start_button = button.Button(
         #     "START", 36, self.width/2, self.height/2)
         exit_button = button.Button(
-            "EXIT", 36, self.width/2, self.height/2 + 50)
+            "EXIT", 36, self.width/2, self.height/2 + 200)
 
         # player number entry buttons
         playerNum_2 = button.Button(
-            "2", 36, self.width/2 - 75, self.height/2)
+            "2", 36, self.width/2 - 75, self.height/1.8)
         playerNum_3 = button.Button(
-            "3", 36, self.width//2 - 25, self.height/2)
+            "3", 36, self.width//2 - 25, self.height/1.8)
         playerNum_4 = button.Button(
-            "4", 36, self.width//2 + 25, self.height/2)
+            "4", 36, self.width//2 + 25, self.height/1.8)
         playerNum_5 = button.Button(
-            "5", 36, self.width//2 + 75, self.height/2)
+            "5", 36, self.width//2 + 75, self.height/1.8)
 
         while True:
 
@@ -51,6 +53,7 @@ class MainMenu():
             # start_button.draw(self.screen)
             exit_button.draw(self.screen)
             text.draw(self.screen)
+            subtitle.draw(self.screen)
 
             playerNum_2.draw(self.screen)
             playerNum_3.draw(self.screen)
