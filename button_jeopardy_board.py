7ce4b1c2525aff1481aebd30efdc7184996c0b0c
caznidarsic <cznidar1@jh.edu>
2022-08-22 10:34:45 -0700

    Fixed empty category round change bug
    - Fixed a bug where if an empty category is selected at the end of a round, the spin count goes negative instead of moving to the next round and resetting

--- button_jeopardy_board.py
+++ button_jeopardy_board.py
@@ -1,46 +1,77 @@
-# import pygame
+import pygame
 
-# # button class
+# button class
 
 
-# class ButtonJeopardyBoard():
-#     def __init__(self,x, y, clickable=True):
-#         white = (255, 255, 255)
-#         blue = (0, 0, 128)
-#         # grey = (115, 115, 115)
-#         self.x_pos = x
-#         self.y_pos = y
-#         self.text_hover = font.render(self.text_input, True, blue)
-#         self.text_unclickable = font.render(self.text_input, True, white)
-#         self.text_unclickable.set_alpha(90)
-#         self.textRect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-#         self.clickable = clickable
+class ButtonJeopardyBoard():
+    def __init__(self, x, y):
+        # store indices
+        self.x = x
+        self.y = y
 
-#         self.clicked = False
+        # initalize colors
+        self.dark_blue = (38, 54, 150)
+        self.light_purple = (190, 25, 207)
+        self.light_blue = (83, 239, 252)
+        self.color = (255, 0, 0)
+        self.color2 = (0, 255, 0)
+        self.white = (255, 255, 255)
 
-#     def draw(self, surface):
-#         action = False
+        # full board size
+        self.full_x_dim = 600
+        self.full_y_dim = 480
 
-#         pos = pygame.mouse.get_pos()
+        # board padding
+        self.x_pad = 10
+        self.y_pad = 10
 
-#         if self.clickable:
-#             if self.textRect.collidepoint(pos):
-#                 if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-#                     self.clicked = True
-#                     action = True
+        # board box sizes
+        self.x_dim = (self.full_x_dim - self.x_pad*8)/6
+        self.y_dim = (self.full_y_dim - self.y_pad*8)/6
 
-#                 if pygame.mouse.get_pressed()[0] == 0:
-#                     self.clicked = False
+        # title alignment
+        self.title_x_start = 950
+        self.title_y_start = 50
+        self.title_y_dim = self.y_dim + self.y_pad*2
 
-#                 surface.blit(self.text_hover, self.textRect)
-#             else:
-#                 surface.blit(self.text, self.textRect)
-#         else:
-#             surface.blit(self.text_unclickable, self.textRect)
-#             self.clicked = False
+        # board alignment
+        self.full_x_start = self.title_x_start
+        self.full_y_start = self.title_y_start + self.title_y_dim + 10
+        self.title_x_dim = self.full_x_dim
 
-#     def setClickable(self, clickableInput):
-#         if clickableInput:
-#             self.clickable = True
-#         elif clickableInput == False:
-#             self.clickable = False
+        # button rect
+        self.buttonRect = pygame.Rect(self.title_x_start+self.x_pad+self.x_dim*self.x+self.x_pad*self.x,
+                                      self.title_y_start+self.y_pad+self.y_dim*self.y+self.y_pad*self.y, self.x_dim, self.full_y_dim-self.y_pad)
+
+        self.clickable = True
+
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+
+        pos = pygame.mouse.get_pos()
+
+        if self.clickable:
+            if self.buttonRect.collidepoint(pos):
+                if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                    self.clicked = True
+                    action = True
+
+                if pygame.mouse.get_pressed()[0] == 0:
+                    self.clicked = False
+
+                rect_obj = pygame.draw.rect(
+                    surface, self.light_purple, self.buttonRect, 6)
+            else:
+                rect_obj = pygame.draw.rect(
+                    surface, self.white, self.buttonRect, 6)
+                self.clicked = False
+        else:
+            self.clicked = False
+
+    # def setClickable(self, clickableInput):
+    #     if clickableInput:
+    #         self.clickable = True
+    #     elif clickableInput == False:
+    #         self.clickable = False
