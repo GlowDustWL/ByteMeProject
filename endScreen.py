880cd63a7c71a87c2d9019a7280b1dfa96539b04
Libby Lewis <elewis43@jh.edu>
2022-08-21 19:15:52 -0500

    Drumroll and celebration music on endscreen

--- endScreen.py
+++ endScreen.py
@@ -21,7 +21,7 @@ class EndScreen():
     # main menu
 
     def getInput(self, numPlayers, playerList, finalScores):
-        playsound('uplifting.mp3', False)
+        #playsound('uplifting.mp3', False)
         text = textMedium.TextMedium(
             "Thank you for Playing!", self.width/2, self.height/2 - 300)
         # self.screen.blit(self.background, (0, 0))
@@ -56,37 +56,68 @@ class EndScreen():
                 str(finalScores[x]), 26, self.width/2 + 100, self.height - 550 + 70*x))
 
         # buttons
-        continue_button = button.Button(
-            "CONTINUE", 32, self.width*(1 - 1/8), self.height - 50)
+        return_to_main = button.Button(
+            "MAIN MENU", 32, self.width*(1 - 1/8), self.height - 50)
+
+        # draw first elements
+        self.screen.blit(self.background, (0, 0))
+        # return_to_main.draw(self.screen)
+        text.draw(self.screen)
+        playerScoreIntro.draw(self.screen)
+        # draw player names/scores
+        for x in nameTextArray:
+            x.draw(self.screen)
+        for x in scoreTextArray:
+            x.draw(self.screen)
+
+        m = pygame.mixer.music
+        m.load('drumroll.wav')
+        m.play()
+        pygame.display.update()
+        pygame.event.pump()
+        # while (m.get_busy()):
+        #    pygame.time.wait(10)
+        m.queue('uplifting.mp3')
+        # m.play()
+        pygame.time.wait(5500)
+        winnerText.draw(self.screen)
+        # pygame.display.update()
+        # pygame.event.pump()
+        # pygame.time.wait(50000)
+
+        # pygame.display.update()
+        # pygame.time.wait(50000)
+        # pygame.display.update()
+        # pygame.mixer.init()
+        #drumroll = pygame.mixer.Sound('drumroll.wav')
+        #congrats = pygame.mixer.Sound('uplifting.mp3')
+        #channel = drumroll.play()
+        # while (channel.get_busy()):
+        #    pygame.time.wait(100)
+        #self.screen.blit(self.background, (0, 0))
+        # congrats.play()
+        # winnerText.draw(self.screen)
+        # pygame.time.wait(1000)
+        # return
+        # pygame.display.update()
 
         loop = True
         while loop:
 
-            # draw elements
-            self.screen.blit(self.background, (0, 0))
-            continue_button.draw(self.screen)
-            text.draw(self.screen)
-            playerScoreIntro.draw(self.screen)
-            winnerText.draw(self.screen)
+            return_to_main.draw(self.screen)
 
-            # draw player names/scores
-            for x in nameTextArray:
-                x.draw(self.screen)
-            for x in scoreTextArray:
-                x.draw(self.screen)
-
-            # event handlers
+        # event handlers
             for event in pygame.event.get():
                 # game window handlers
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-                if continue_button.clicked:
+                if return_to_main.clicked:
                     return
 
-                # other handlers
-                # ...
+        # other handlers
+        # ...
 
-            # update the game state
+        # update the game state
             pygame.display.update()
             self.clock.tick(60)
